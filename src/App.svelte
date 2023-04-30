41bb1b7dc4fb58cd2ac01068463d0a4a80952c62
RadekMocek <radek.mocek@tul.cz>
2023-05-01 00:55:30 +0200

    Login page

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,14 @@
 <script>
     import Home from "./lib/Home.svelte";
+    import Login from "./lib/Login.svelte";
     import Router from "svelte-spa-router";
 
     const routes = {
-        "/": Home
+        "/": Home,
+        "/login": Login,
     };
 </script>
 
 <main>
     <Router {routes} />
 </main>
-
--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -2,9 +2,10 @@
     import moneyMouthFace from "/money-mouth-face.png";
     import bank from "/bank.png";
 
+    import { push } from "svelte-spa-router";
     import { onMount } from "svelte";
 
-    let isTokenInStorage = false;    
+    let isTokenInStorage = false;
 
     onMount(() => {
         let token = localStorage.getItem("jwt");
@@ -17,19 +18,23 @@
 {#if isTokenInStorage}
     <p>Přihlášen</p>
 {:else}
-    <div>
+    <div id="content">
         <img src={bank} class="logo" alt="Bank Logo" />
         <img src={moneyMouthFace} class="logo" alt="Bank Logo" />
-    </div>
-
-    <h1>STIN Bank</h1>
-
-    <div class="card">
-        <button>Přihlásit se</button>
+        
+        <h1>STIN Bank</h1>
+        
+        <div class="card">
+            <button on:click={() => push("/login")}>Přihlásit se</button>
+        </div>
     </div>
 {/if}
 
 <style>
+    #content {
+        text-align: center;
+    }
+
     .logo {
         height: 6em;
         padding: 1.5em;
